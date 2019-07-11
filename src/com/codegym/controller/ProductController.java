ff2a54a1e554e9c5d10c1cf84538aaa1c1be7c8e
Your Name <you@example.com>
2019-07-11 17:11:08 +0700

    'add'

---
+++ src/com/codegym/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.ProductService;
+
+public class ProductController {
+    ProductService productService = new ProductService();
+
+    public Product[] showList() {
+        return productService.showList();
+    }
+
+    public int size() {
+        return productService.size();
+    }
+
+    public void add(int index, Product Product) {
+        productService.add(index, Product);
+    }
+
+    public void remove(int index) {
+        productService.remove(index);
+    }
+
+    public Product set(int index, Product Product) {
+        return productService.set(index, Product);
+    }
+
+    public int find(String ProductName) {
+        return productService.find(ProductName);
+    }
+    public Product[] sort() {
+        return productService.sort();
+    }
+
+}
